5c94d708a5edc31fe6c9e15e71d50c5b8e4ec820
Akash <amali3741@gmail.com>
2025-05-17 23:32:36 +0530

    creating project

---
+++ src/main/java/com/restfulbooker/payloads/pojos/BookingDates.java
@@ -0,0 +1,32 @@
+package com.restfulbooker.payloads.pojos;
+
+public class BookingDates {
+
+    private String checkin;
+    private String checkout;
+
+    // Getters and setters
+    public String getCheckin() {
+        return checkin;
+    }
+
+    public void setCheckin(String checkin) {
+        this.checkin = checkin;
+    }
+
+    public String getCheckout() {
+        return checkout;
+    }
+
+    public void setCheckout(String checkout) {
+        this.checkout = checkout;
+    }
+
+    @Override
+    public String toString() {
+        return "BookingDates{" +
+                "checkin='" + checkin + '\'' +
+                ", checkout='" + checkout + '\'' +
+                '}';
+    }
+}
